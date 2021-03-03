0e7a7066e899963b8ff360a1d45c69bf8a4d0bb1
Munir Mahmud <munirmahmud6@gmail.com>
2021-03-03 23:54:06 +0600

    Database return type added as Collection

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,8 +1,9 @@
 import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER}.8vnfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
