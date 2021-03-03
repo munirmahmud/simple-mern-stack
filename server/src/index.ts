e8752c1a6cfb0f6b506af3bc06652f6d596fc993
Munir Mahmud <munirmahmud6@gmail.com>
2021-03-03 22:38:10 +0600

    Apollo server initialized

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
 const app = express();
 const port = 9000;
+
+const server = new ApolloServer({});
+server.applyMiddleware({ app, path: "/api" });
 
 app.get("/", (_req, res) => res.send("Home Page"));
 
