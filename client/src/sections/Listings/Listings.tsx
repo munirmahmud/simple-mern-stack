cd402f9df840e5eb09dcc2db39a249ca37d968b2
Munir Mahmud <munirmahmud6@gmail.com>
2021-03-05 17:36:51 +0600

    Abstracting the type of data from server fetch

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { server } from "../../lib/api";
+import { ListingsData } from "./types";
 
 const LISTINGS = `
   query Listings {
@@ -19,8 +20,8 @@ const LISTINGS = `
 
 export const Listings = () => {
   const handleClick = async () => {
-    const { data } = await server.fetch({ query: LISTINGS });
-    console.log(data);
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data.listings);
   };
 
   return (
