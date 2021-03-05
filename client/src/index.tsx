3771214a60750a5efdfe3edac1af93529e575299
Munir Mahmud <munirmahmud6@gmail.com>
2021-03-05 08:16:55 +0600

    Listing component initialized

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import { Listings } from "./sections";
 
-render(
-  <React.StrictMode>
-    <h2>Hello React!</h2>,
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+render(<Listings />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
